ed23c60410b674d5d6d5c92cc1f755da2081a77f
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-10-16 08:59:12 +0400

    random stuff

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -3,10 +3,28 @@ use std::io::{stdout, BufWriter};
 use ferris_says::say;
 
 fn main() {
+    println!("wowie");
     let stdout = stdout();
     let message = String::from("hi!! hello!");
     let width = message.len();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+
+    let x = 234;
+    println!("printing value: {x}");
+
+    let y;
+    if x == 235 {
+        y = "banana";
+    } else {
+        y = "nonana";
+    };
+    
+    println!("what?: {y}");
+
+    let mut z = 44;
+    println!("mutable: {z}");
+    z = 234;
+    println!("mutable 2: {z}");
 }
