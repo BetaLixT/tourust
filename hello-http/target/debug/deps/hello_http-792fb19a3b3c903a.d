292df0f317addce8d9e426ef1be3e2becf385312
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-10-22 16:14:44 +0400

    wip http handling

---
+++ hello-http/target/debug/deps/hello_http-792fb19a3b3c903a.d
@@ -0,0 +1,5 @@
+/home/dcruza/Projects/personal/tourust/hello-http/target/debug/deps/hello_http-792fb19a3b3c903a: src/main.rs
+
+/home/dcruza/Projects/personal/tourust/hello-http/target/debug/deps/hello_http-792fb19a3b3c903a.d: src/main.rs
+
+src/main.rs:
---
+++ hello-http/target/debug/hello-http.d
@@ -0,0 +1,1 @@
+/home/dcruza/Projects/personal/tourust/hello-http/target/debug/hello-http: /home/dcruza/Projects/personal/tourust/hello-http/src/main.rs
