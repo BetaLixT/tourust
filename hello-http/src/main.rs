328620053560d3dd858380a8326a65c7d366cd47
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-10-22 17:46:50 +0400

    serve html

--- hello-http/src/main.rs
+++ hello-http/src/main.rs
@@ -3,6 +3,20 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+const HOME_CONTENT: &str = r#"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust</p>
+  </body>
+</html>
+"#;
+
 fn main() {
     println!("Hello, world!");
     let listener = TcpListener::bind("localhost:8082").unwrap();
@@ -22,7 +36,12 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let res = "HTTP/1.1 200 ok\r\n\r\n";
-    stream.write_all(res.as_bytes()).unwrap();
+    let status_line = "HTTP/1.1 200 ok";
+    let len = HOME_CONTENT.len();
+    stream
+        .write_all(
+            format!("{status_line}\r\nContent-Length: {len}\r\n\n\r{HOME_CONTENT}").as_bytes(),
+        )
+        .unwrap();
     println!("Request: {:#?}", http_request);
 }
