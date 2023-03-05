cd4412e8287e54ea35e7b5e382443c930ac5f4f0
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-03-05 10:19:24 +0400

    wip

--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,12 +1,27 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::{json::Json, Deserialize};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn home(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Movie<'r> {
+    title: &'r str,
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![home])
 }
