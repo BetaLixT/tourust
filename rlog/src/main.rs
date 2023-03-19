ebe2db29f07b3f718af5a403c2b64465a9cc2257
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-03-19 18:05:35 +0400

    wip

--- rlog/src/main.rs
+++ rlog/src/main.rs
@@ -1,29 +1,49 @@
-use std::{sync::mpsc, thread};
+use std::{str::FromStr, sync::mpsc, thread};
 
 fn main() {
     println!("Hello, world!");
-    let lgr = RLog {};
+    let lgr = new_rlog();
     lgr.debug("debug log!");
     lgr.info("info log!");
     lgr.warn("warn log!");
     lgr.error("error log!");
+    lgr.close();
 }
 
 struct RLog {
     queue_processor_thread_join: thread::JoinHandle<()>,
+    sender: mpsc::Sender<String>,
+}
+
+fn new_rlog() -> RLog {
+    let (sender, receiver): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
+    RLog {
+        queue_processor_thread_join: process_queue(receiver),
+        sender,
+    }
 }
 
 impl RLog {
-    fn debug(&self, log: &str) {}
-    fn info(&self, log: &str) {}
-    fn warn(&self, log: &str) {}
-    fn error(&self, log: &str) {}
-
-    fn process_queue(&self) {
-        let (sender, receiver): (mpsc::Sender<&str>, mpsc::Receiver<&str>) = mpsc::channel();
-        self.queue_processor_thread_join = thread::spawn(|| {});
+    fn debug(&self, log: &str) {
+        self.sender.send(String::from_str(log).unwrap());
     }
-    fn close(&self) {
+    fn info(&self, log: &str) {
+        self.sender.send(String::from_str(log).unwrap());
+    }
+    fn warn(&self, log: &str) {
+        self.sender.send(String::from_str(log).unwrap());
+    }
+    fn error(&self, log: &str) {
+        self.sender.send(String::from_str(log).unwrap());
+    }
+    fn close(self) {
         self.queue_processor_thread_join.join();
     }
+}
+
+fn process_queue(recv: mpsc::Receiver<String>) -> thread::JoinHandle<()> {
+    thread::spawn(|| {
+        let log = recv.recv().unwrap();
+        println!("{}", log);
+    })
 }
