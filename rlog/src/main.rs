973112a10c909bb17e73ce6965492d238e03b05a
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-03-19 16:57:41 +0400

    wip

--- rlog/src/main.rs
+++ rlog/src/main.rs
@@ -1,14 +1,29 @@
+use std::{sync::mpsc, thread};
+
 fn main() {
     println!("Hello, world!");
     let lgr = RLog {};
-    lgr.Debug("");
+    lgr.debug("debug log!");
+    lgr.info("info log!");
+    lgr.warn("warn log!");
+    lgr.error("error log!");
 }
 
-struct RLog {}
+struct RLog {
+    queue_processor_thread_join: thread::JoinHandle<()>,
+}
 
 impl RLog {
-    fn Debug(&self, log: &str) {}
-    fn Info(&self) {}
-    fn Warn(&self) {}
-    fn Error(&self) {}
+    fn debug(&self, log: &str) {}
+    fn info(&self, log: &str) {}
+    fn warn(&self, log: &str) {}
+    fn error(&self, log: &str) {}
+
+    fn process_queue(&self) {
+        let (sender, receiver): (mpsc::Sender<&str>, mpsc::Receiver<&str>) = mpsc::channel();
+        self.queue_processor_thread_join = thread::spawn(|| {});
+    }
+    fn close(&self) {
+        self.queue_processor_thread_join.join();
+    }
 }
